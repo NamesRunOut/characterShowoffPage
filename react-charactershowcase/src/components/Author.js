0d91cfdcadb835fabff6036b5dd11dd55f729514
vvyvi <ewawozny4@gmail.com>
2020-08-30 00:29:04 +0200

    original commit

--- react-charactershowcase/src/components/Author.js
+++ react-charactershowcase/src/components/Author.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 function Author() {
     return(
         <div className="footer" id="author">
+            Not meant to be viewed on mobile, it's not there yet!
             Top video clip comes from one of my League of Legends games.
             Background images used for this project are a property of Riot Games, Inc.
             <br />Text used in ability descriptions mostly comes from the following: 
